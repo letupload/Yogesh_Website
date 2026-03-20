efce6134a270f93a8ba01cc942a77408bbd55b84
letupload <it.b.12.yogeshgovindnalawade@gmail.com>
2026-03-20 13:03:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing",{
+strings:["Web Developer","Java Developer","AI Enthusiast"],
+typeSpeed:60,
+backSpeed:40,
+loop:true
+});
+
+particlesJS.load('particles-js','particles.json');
